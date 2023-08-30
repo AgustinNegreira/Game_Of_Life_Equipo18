4f4dc1dbdb094c67c1391d73ce6aa33dece6b96a
Bmorat <brian.morat@correo.ucu.edu.uy>
2023-08-30 20:38:30 -0300

    Cambio

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace PII_Game_Of_Life
 {
@@ -6,7 +8,33 @@ namespace PII_Game_Of_Life
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         
+
+        }
+public class Lector
+{
+    public Lector()
+    {
+        string url = "ruta del archivo";
+        string content = File.ReadAllText(url);
+        string[] contentLines = content.Split('\n');
+        bool[,] board = new bool[contentLines.Length, contentLines[0].Length];
+
+        for (int y = 0; y < contentLines.Length; y++)
+        {
+            for (int x = 0; x < contentLines[y].Length; x++)
+            {
+                if (contentLines[y][x] == '1')
+                {
+                    board[y, x] = true;
+                }
+            }
         }
     }
+}
+
+
+    }
+
+
 }
