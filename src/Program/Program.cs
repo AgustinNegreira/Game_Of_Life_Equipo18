5e7d7d8ddf7725861346831c4df997337beac958
Bmorat <brian.morat@correo.ucu.edu.uy>
2023-09-05 23:35:13 -0300

    Game of Life

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,14 +1,35 @@
 ﻿using System;
+<<<<<<< Updated upstream
 using System.IO;
 using System.Text;
+=======
+using Library;
+>>>>>>> Stashed changes
 
-namespace PII_Game_Of_Life
+namespace GameOfLife
 {
     class Program
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
          
+=======
+            string archivoTablero = @"C:\Users\bmora\OneDrive\Escritorio\C#\juego1\board.txt"; // Cambia esto por la ruta correcta
+
+            LectorArchivo lector = new LectorArchivo();
+            Tablero tablero = lector.LeerArchivo(archivoTablero);
+
+            JuegoDeLaVida juego = new JuegoDeLaVida();
+            ImprimirTablero impresor = new ImprimirTablero();
+
+            while (true)
+            {
+                impresor.MostrarTablero(tablero);
+                juego.AvanzarGeneracion(tablero);
+                System.Threading.Thread.Sleep(300); // Pausa de 300 milisegundos
+            }
+>>>>>>> Stashed changes
         }
 
     }
